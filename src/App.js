e64f6bd42649f8cf3a14fa67dfd25d30747e9608
rachel <rachel@seedsgives.com>
2023-05-14 14:12:33 -0400

    Adding all dependency modifications to make the Stripe API work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Nav from './Nav';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <Nav />
+      <header>
+        <h2>Date Received</h2>
+        <h2>Net Gift Amount</h2>
       </header>
+      {payouts.data.map((data) => {
+
+        let date = new Date(data.arrival_date * 1000)
+
+        let amount = data.amount / 100;
+
+        return (
+          <ul className="columns">
+            <li>{date.toLocaleDateString("en-US")}</li>
+            <li>{amount}</li>
+          </ul>
+        )
+      })};
     </div>
   );
 }
+
+const stripe = require('stripe')('sk_test_51HTHi2Hvy5NeCPEsFMyNF55MzJSDciOw2Qmp4KS4DLaf51eFUxDQcDu4dHfOc7PcqmyTbFHfKh3GhjhVDfmmWE21009ZNmVRCE');
+
+const payouts = await stripe.payouts.list({
+  limit: 1000
+});
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+
+const Nav = () => {
+    return (
+        <nav>
+            <img className="logo" src="https://images.squarespace-cdn.com/content/v1/5bf36b4936099bd154911b7e/1592428554342-PP47P5BP6DD7WML6KW13/Seeds+High+Res+Logo.png?format=1500w" alt="Green Seeds logo" />
+            <h1>Seeds Gifts To Those in Need 💗</h1>
+        </nav>
+    )
+}
+
+export default Nav;
