da7e0832723329b5e677fe09fa4b99cfce1af0fc
rachel <rachel@seedsgives.com>
2023-05-17 11:40:04 -0400

    Cleaning up the code, & trying to filter data to pass from parent to child

--- src/App.js
+++ src/App.js
@@ -2,34 +2,22 @@
 import './App.css';
 import Nav from './components/Nav';
 import Form from './components/Form';
-// import { useEffect, useState } from 'react';
 
+// calling the Stripe API outside of the function and outside of useEffect because can't figure out how to get await/using a promise to work inside of those
 const stripe = require('stripe')('sk_test_51HTHi2Hvy5NeCPEsFMyNF55MzJSDciOw2Qmp4KS4DLaf51eFUxDQcDu4dHfOc7PcqmyTbFHfKh3GhjhVDfmmWE21009ZNmVRCE');
 
 const payouts = await stripe.payouts.list({
   limit: 1000
 });
 
+payouts.data.map((data) => {
+
+  let date = new Date(data.arrival_date * 1000)
+  let amount = data.amount / 100;
+
+})
+
 function App() {
-
-  // const [date, setDate] = useState();
-
-  // useEffect(() => {
-  //   //handling the Stripe API call
-  //   const stripe = require('stripe')('sk_test_51HTHi2Hvy5NeCPEsFMyNF55MzJSDciOw2Qmp4KS4DLaf51eFUxDQcDu4dHfOc7PcqmyTbFHfKh3GhjhVDfmmWE21009ZNmVRCE');
-
-  //   const payouts = await stripe.payouts.list({
-  //     limit: 1000
-  //   })
-  //   .then(res => {
-  //     //storing the API data
-  //     const apiResults = res.payouts.data;
-  //     //map over the original API results array to extract the two pieces of data we want (?)
-
-  //     const dateAndAmount = apiResults.map
-  //   })
-
-  // })
 
   return (
     <div className="App">
@@ -44,13 +32,11 @@ function App() {
         let date = new Date(data.arrival_date * 1000)
         let amount = data.amount / 100;
 
-        // console.log(date);
-        // console.log(amount);
-        // console.log(payouts.data);
+        console.log(date, amount);
 
         return (
           <ul className="columns">
-            {/* how do i access the id within the array to address the below console error? */}
+            {/* how do i access the id, which is within an object within the array, to address the below console error?  data.id in line 46 isn't working */}
             {/* {
               payouts.data.map((data) => {
                 return
@@ -67,21 +53,5 @@ function App() {
     </div>
   );
 }
-
-// useEffect(() => {
-
-//   const stripe = require('stripe')('sk_test_51HTHi2Hvy5NeCPEsFMyNF55MzJSDciOw2Qmp4KS4DLaf51eFUxDQcDu4dHfOc7PcqmyTbFHfKh3GhjhVDfmmWE21009ZNmVRCE');
-
-//   const payouts = await stripe.payouts.list({
-//     limit: 1000
-//   }
-
-//     payouts();
-
-//   );
-// })
-//   .then(res => {
-
-//   })
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,29 +3,16 @@ import { useState } from "react";
 const Form = () => {
 
     //Create a stateful variable to hold the user's date choice
-    const [chosenDate, setChosenDate] = useState(); //should what's inside the () be a string because it'll be a date inputted in the date form? 
+    const [chosenDate, setChosenDate] = useState();
+    const [ records, setRecords ] = useState(chosenDate);
 
-    console.log(chosenDate);
-
-    // useEffect(() => {
-    //     //can I filter the API date already returned by the API call in App.js?
-
-
-    //     // const stripe = require('stripe')('sk_test_51HTHi2Hvy5NeCPEsFMyNF55MzJSDciOw2Qmp4KS4DLaf51eFUxDQcDu4dHfOc7PcqmyTbFHfKh3GhjhVDfmmWE21009ZNmVRCE');
-
-    //     // const payouts = await stripe.payouts.list({
-    //     //     limit: 1000
-    //     // });
-    //     // .then((res) => {
-
-    //     //     setChosenDate(response.data.arrival_date * 1000);
-
-    //     // });
-    //     console.log(chosenDate);
-    // });
+    const filter = (e) => {
+        setRecords(chosenDate.filter(f => f.data.arrival_date().includes(e.target.value)))
+    }
 
     return (
         //Credit: got help figuring out how to hold a selected date in state from this tutorial: https://www.youtube.com/watch?v=dobnC4p5PYw
+
         <>
             <div className="form">
                 <form>
@@ -33,11 +20,22 @@ const Form = () => {
                     <input className="form" type="date" id="date"
                         placeholder="dd-mm-yyyy"
                         min="2013-02-22"
-                        max="2023-12-31" onChange={e => setChosenDate(e.target.value)} />
+                        max="2023-12-31" onChange={filter} />
                 </form>
             </div>
-            {/* how do I then get React to filter the data to print the ChosenDate to the page? */}
         </>
+
+        // <>
+        //     <div className="form">
+        //         <form onSubmit={(e) => displayData(e, chosenDate)}>
+        //             <label htmlFor="date">Please  select the date you'd like to view:</label>
+        //             <input className="form" type="date" id="date"
+        //                 placeholder="dd-mm-yyyy"
+        //                 min="2013-02-22"
+        //                 max="2023-12-31" onChange={e => setChosenDate(e.target.value)} />
+        //         </form>
+        //     </div>
+        // </>
     )
 }
 
