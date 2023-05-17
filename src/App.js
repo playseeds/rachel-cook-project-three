a686393aa16fded253c6e70154e07542973f2a74
rachel <rachel@seedsgives.com>
2023-05-17 17:14:25 -0400

    Fixing filtering issues

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Nav from './components/Nav';
 import Form from './components/Form';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // calling the Stripe API outside of the function and outside of useEffect because can't get await/using a promise to work inside of those (not something we covered in class)
 const stripe = require('stripe')('sk_test_51HTHi2Hvy5NeCPEsFMyNF55MzJSDciOw2Qmp4KS4DLaf51eFUxDQcDu4dHfOc7PcqmyTbFHfKh3GhjhVDfmmWE21009ZNmVRCE');
@@ -13,19 +13,39 @@ const payouts = await stripe.payouts.list({
 
 function App() {
 
-  //Credit: Tried to adapt Esther's recent Unsplash API lesson to make the below work.
+  //Creating stateful variables to hold the user's date choice in the form
+  const [apiResults, setApiResults] = useState(payouts.data);
+  const [chosenDate, setChosenDate] = useState(""); //without the string, initial value is undefined, which can toss errors
+  const [filteredData, setFilteredData] = useState([]);
 
-  //Creating a stateful variable to hold the user's date choice in the form
-  const [chosenDate, setChosenDate] = useState(); 
+  useEffect(() => {
+
+    if (chosenDate === "") {
+      setFilteredData(apiResults)
+    } else {
+      const filteredArray = apiResults.filter((payment) => {
+        const dateConversion = new Date(payment.arrival_date * 1000 - 5).toISOString().split("T")[0];
+
+        return dateConversion === chosenDate
+
+      })
+
+      setFilteredData(filteredArray);
+
+    }
+
+  }, [chosenDate, apiResults])
+
+  //Credit: Tried to adapt Esther's recent Unsplash API lesson to make the below work.
 
   //this function is meant to handle form submission & filtering the date choice and its related amount
   const getDate = (event, chosenDate) => {
     event.preventDefault();
 
     //trying to loop over the date array to return only the data for the date selected in the form
-    
+
     const filteredDate = chosenDate.filter
-    
+
     // (correctSelection => {
     //   return date === chosenDate
     // })
@@ -37,13 +57,13 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Form setChosenDate />
+      <Form setChosenDate={setChosenDate} />
       <header>
         <h2>Date Received</h2>
         <h2>Net Gift Amount (in USD)</h2>
       </header>
 
-      {payouts.data.map((data) => {
+      {filteredData.map((data) => {
 
         let date = new Date(data.arrival_date * 1000)
         let amount = data.amount / 100;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,9 @@
-// import { useState } from "react";
-// import ErrorMessage from "./ErrorMessage";
+import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
 
-const Form = (setChosenDate) => {
+const Form = ({setChosenDate}) => {
+
+    
 
     return (
         //Credit: got help figuring out how to hold a selected date in state from this tutorial: https://www.youtube.com/watch?v=dobnC4p5PYw
@@ -14,7 +16,14 @@ const Form = (setChosenDate) => {
                         min="2013-02-22"
                         max="2023-12-31" onChange={e => setChosenDate(e.target.value)} />
                 </form>
-                {/* is there where I'd get React to display setChosenDate on the page? */}
+                {/* is there where I'd get React to display setChosenDate on the page, or better to do in App? */}
+
+                {/* {if (chosenDate === date) {
+
+                } else {
+                    <ErrorMessage />
+                }} */}
+
             </div>
         </>
     )
